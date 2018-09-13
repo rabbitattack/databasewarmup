61984dbcfce7ac694057cc68f57373b01183fbd6
Jedi77 <jprentup@hotmail.com>
2018-09-13 14:18:52 -0600

    a good push

--- migrations/20180913091826_cohortTable.js
+++ migrations/20180913091826_cohortTable.js
@@ -5,11 +5,11 @@ exports.up = (knex, Promise) => {
         student.string('lastName')
         student.string('hometown')
         student.string('prevOccupation')
-        student.string('favoriteNumber')
+        student.string('favoriteNum')
         student.string('pastime')
     })
 };
 
 exports.down = function (knex, Promise) {
-    return knex.schema.dropTableIfExists
+    return knex.schema.dropTableIfExists('cohortTable')
 };
